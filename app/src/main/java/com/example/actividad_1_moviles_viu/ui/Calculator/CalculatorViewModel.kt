c767163189738a3158bb5757874821b3ff15f5b3
IsaacPilatuna <isapila1996@hotmail.com>
2022-04-17 16:48:06 -0500

    Needing components

--- app/src/main/java/com/example/actividad_1_moviles_viu/ui/Calculator/CalculatorViewModel.kt
+++ app/src/main/java/com/example/actividad_1_moviles_viu/ui/Calculator/CalculatorViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.actividad_1_moviles_viu.ui.dashboard
+package com.example.actividad_1_moviles_viu.ui.Calculator
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class DashboardViewModel : ViewModel() {
+class CalculatorViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+        value = "This is home Fragment"
     }
     val text: LiveData<String> = _text
 }
